dfed61a8900a055af1c29750840305e6e974380a
morrowi <morrowi@ya.ru>
2022-07-01 02:49:36 +0300

    01.07.22

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-sm-12 form-group">
+      <label>Please search an address</label>
+      <input
+          class="form-control"
+          type="text"
+          v-model="address_input"
+          @keyup="get_addresses_debounced"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import _ from "lodash";
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+
+  },
+  data(){
+    return{
+      address_input: null,
+      get_addresses_debounced: null
+    }
+  },
+  methods:{
+    async get_addresses() {
+      this.loading = true;
+      let response = [];
+      try {
+        // Comment this line and uncomment the next one to make local api calls
+        response = await axios.get(
+            `https://nominatim.openstreetmap.org/search?city=${this.address_input}&format=json&addressdetails=0`
+        );
+        // response = await axios.get("./index.json");
+      } catch (error) {
+        console.error(error);
+      }
+      console.log(response.data);
+
+      this.polygon(response.data[0].lat,response.data[0].lon);
+
+    },
+    polygon(lat, lon){
+      axios.post( 'https://script.kdm1.ru/skystrim/api.php',
+          {lat:lat, lon:lon},
+      ).then((resp)=>{
+        console.log(resp.data);
+
+      }).catch(function(error){
+        console.log(error);
+      });
+    }
+  },
+  created() {
+    this.get_addresses_debounced = _.debounce(this.get_addresses, 1000);
+  },
 }
 </script>
 
